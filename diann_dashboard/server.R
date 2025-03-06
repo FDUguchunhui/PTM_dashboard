a9298bd1b1557b8f37aa90cb854bc9e9f7f13d04
Chunhui Gu <fduguchunhui@gmail.com>
2025-03-06 11:48:03 -0600

    use R6 class

--- diann_dashboard/server.R
+++ diann_dashboard/server.R
@@ -13,6 +13,8 @@ metadata <- read_csv("../data/metadata.csv")
 
 source('../R/helper_functions.R')
 source('../R/PTM_definition.R')
+source('../R/data_object.R')
+source('../R/normalization.R')
 
 # ----------------- Helper Functions -----------------
 
@@ -24,34 +26,34 @@ load_data <- function(ptm_choice) {
   df %>%
     mutate(
       has_target_PTM = str_detect(Modified.Sequence, paste0('\\(', ptm_info$symbol, '\\)', '|', '\\(', ptm_info$unimod, '\\)')),
-      has_target_site = str_detect(Stripped.Sequence, ptm_info$site),
-      num_PTM = str_count(Modified.Sequence, fixed(ptm_info$symbol))
+      has_target_site = str_detect(Stripped.Sequence, ptm_info$site)
+      # num_PTM = str_count(Modified.Sequence, fixed(ptm_info$symbol))
     )
 }
 
 # ----------------- Shiny Server -----------------
 
 server <- function(input, output, session) {
-  
+
   # Use reactiveVal for processed_data to allow manual updates
   raw_data <- reactiveVal(NULL)
-  
+
   # Manually update processed_data when input$file_dropdown changes
   observeEvent(input$file_dropdown, {
     req(input$file_dropdown)
     new_data <- load_data(input$file_dropdown)
     raw_data(new_data)
-    
+
     # Update Cancer Type Dropdown based on new processed data
     cancer_types <- unique(metadata$`Cancer Type`[!is.na(metadata$`Cancer Type`)])
     updateSelectInput(session, "cancer_type_dropdown",
                       choices = cancer_types, selected = c('Breast', 'LEAP Control', 'MERIT Control'))
-    
+
     updateSelectInput(session, "cancer_type_dropdown_output",
                       choices = cancer_types, selected = c('Breast', 'LEAP Control', 'MERIT Control'))
   })
-  
-  
+
+
   processed_data <- reactive({
     req(raw_data())
     PTM_filtered <- raw_data() %>%
@@ -62,21 +64,22 @@ server <- function(input, output, session) {
       filter(!has_target_PTM)
     rbind(PTM_filtered, normal_data)
   })
-  
+
   # Total intensity data (log10 transformed)
   total_intensity_data <- reactive({
     req(processed_data())
+
     processed_data() %>%
       group_by(plate, ID, assay, evotip, well, has_target_PTM) %>%
       summarise(
         total_intensity = sum(Precursor.Quantity, na.rm = TRUE)) %>%
       ungroup() %>%
       mutate(log_total_intensity = log10(total_intensity)) %>%
-      inner_join(metadata, by = "ID", suffix = c("", ".meta")) %>% 
-      select(-ends_with(".meta")) %>% 
+      inner_join(metadata, by = "ID", suffix = c("", ".meta")) %>%
+      select(-ends_with(".meta")) %>%
       filter(!is.na(`Cancer Type`))
   })
-  
+
   # Protein-level aggregation
   protein_level_aggregation <- reactive({
     req(processed_data())
@@ -86,9 +89,9 @@ server <- function(input, output, session) {
       summarise(
         has_target_PTM = any(has_target_PTM), .groups='drop') %>%
       inner_join(metadata, by = "ID")
-      
+
   })
-  
+
   # Peptide-level aggregation
   peptide_level_aggregation <- reactive({
     req(processed_data())
@@ -99,13 +102,13 @@ server <- function(input, output, session) {
                 total_intensity = sum(Precursor.Quantity), .groups='drop') %>%
       inner_join(metadata, by = "ID")
   })
-  
-  
+
+
   # modified-peptide level
   modified_peptide_level <- reactive({
 
     dt <- as.data.table(processed_data())  # Convert to data.table
-    
+
     dt[, .(
         Genes = Genes[1],
         Protein.Group = Protein.Group[1],
@@ -113,52 +116,65 @@ server <- function(input, output, session) {
         has_target_PTM = has_target_PTM[1],
         total_intensity = sum(Precursor.Quantity, na.rm = TRUE)
       ), by = .(plate, ID, assay, evotip, well, Modified.Sequence)]
-    
+
       })
-  
+
   wide_format <- reactive({
     req(modified_peptide_level())
-    
-    modified_peptide_level() %>% 
+
+    modified_peptide_level() %>%
       # rename has_target_PTM to TARGET_PTM[[input$file_dropdown]]$name
       rename(!!TARGET_PTM[[input$file_dropdown]]$name := has_target_PTM) %>%
       # mutate(total_intensity = log10(total_intensity)) %>%
-      tidyr::pivot_wider(id_cols=c(Protein.Group, Protein.Ids, Genes, Modified.Sequence, TARGET_PTM[[input$file_dropdown]]$name), 
+      tidyr::pivot_wider(id_cols=c(Protein.Group, Protein.Ids, Genes, Modified.Sequence, TARGET_PTM[[input$file_dropdown]]$name),
                   names_from = c(plate, ID, assay, evotip, well),
                   values_from = total_intensity, values_fill = 0)
   })
-  
+
+  annotated_data <- reactive({
+    dat <- wide_format() %>% select(-c(Protein.Group, Protein.Ids, Genes, TARGET_PTM[[input$file_dropdown]]$name))
+    dat_matrix <- as.matrix(dat[-1])
+    rownames(dat_matrix) <- dat$Modified.Sequence
+    # create row metadata
+    row_metadata <- wide_format()[, c('Modified.Sequence', 'Protein.Group', 'Protein.Ids', 'Genes', TARGET_PTM[[input$file_dropdown]]$name)]
+
+    meta <- generated_metadata()
+    # filter dat_matrix only keep those in metadata
+    dat_matrix <- dat_matrix[, colnames(dat_matrix) %in% meta$Run]
+    # Step 2: Reorder metadata based on column order in dat_matrix
+    ordered_meta <- meta[match(colnames(dat_matrix), meta$Run), ]
+    annotated_dat <- AnnotatedData$new(dat_matrix,
+                                   col_metadata=ordered_meta,
+                                   row_metadata=row_metadata)
+  })
+
   generated_metadata <- reactive({
     req(modified_peptide_level())
-    
-    metadata2  <- 
-      modified_peptide_level()  %>% 
-      select(plate, ID, assay, evotip, well) %>%
-      unique() %>%
-      mutate(Run=paste(plate, ID, assay, evotip, well, sep='_')) %>% 
-      inner_join(metadata, by='ID')
-    
-    # get Run ID in input$cancer_type_dropdown_output
-    metadata2 %>% 
-      filter(`Cancer Type` %in% input$cancer_type_dropdown_output)
-  }) 
-  
-  
+
+    modified_peptide_level()  %>%
+    select(plate, ID, assay, evotip, well) %>%
+    unique() %>%
+    mutate(Run=paste(plate, ID, assay, evotip, well, sep='_')) %>%
+    inner_join(metadata, by='ID')
+
+  })
+
+
   # ----------------- Plot Outputs -----------------
-  
+
   # Plot 1: Histogram of total intensity (combined case/control label)
-  output$matplotlib_plot <- renderPlotly({
+  output$intensity_plot <- renderPlotly({
     req(total_intensity_data(), input$file_dropdown)
     ptm_info <- TARGET_PTM[[input$file_dropdown]]
-    
+
     plot_data <- total_intensity_data() %>%
       filter(!is.na(group)) %>%
       mutate(ptm_label = ifelse(has_target_PTM, "Modified peptide", "Unmodified peptide"),
              combined = paste(group, ptm_label, sep = ", "))
-    
+
     plot_title <- paste("Distribution of total intensity of peptide with",
                         ptm_info$name, "on site", ptm_info$site)
-    
+
     create_histogram_plot(plot_data,
                           x_var = "log_total_intensity",
                           fill_var = "combined",
@@ -167,30 +183,30 @@ server <- function(input, output, session) {
                           xlab = "Total intensity (log10)",
                           ylab = "Frequency")
   })
-  
+
   # Plot 2: Protein-level case–control plot
   output$prot_prop_plot_case_control <- renderPlotly({
     req(protein_level_aggregation(), input$file_dropdown)
     ptm_info <- TARGET_PTM[[input$file_dropdown]]
     plot_case_control(protein_level_aggregation(), metadata, ptm_info, type = "protein", bins = 50)
   })
-  
+
   # Plot 3: Peptide-level case–control plot
   output$pep_prop_plot_case_control <- renderPlotly({
     req(peptide_level_aggregation(), input$file_dropdown)
     ptm_info <- TARGET_PTM[[input$file_dropdown]]
     plot_case_control(peptide_level_aggregation(), metadata, ptm_info, type = "peptide", bins = 50)
   })
-  
+
   # Plot 4: Histogram of total intensity filtered by Cancer Type
-  output$histogram_plot <- renderPlotly({
+  output$intensity_plot_cancer_type <- renderPlotly({
     req(total_intensity_data(), input$cancer_type_dropdown, input$file_dropdown)
-    
+
     plot_data <- total_intensity_data() %>%
       filter(has_target_PTM,
              `Cancer Type` %in% input$cancer_type_dropdown,
              assay %in% c("FT", "IgB"))
-    
+
     plot_title <- paste("Distribution of Total Intensity with", input$file_dropdown)
     create_histogram_plot(plot_data,
                           x_var = "log_total_intensity",
@@ -201,20 +217,20 @@ server <- function(input, output, session) {
                           ylab = "Frequency",
                           facet_formula = "~ assay")
   })
-  
-  
+
+
   # Plot5: Peptide-level histogram by Cancer Type
   output$pep_prop_plot_cancer_type <- renderPlotly({
     req(peptide_level_aggregation(), input$cancer_type_dropdown, input$file_dropdown)
     ptm_info <- TARGET_PTM[[input$file_dropdown]]
-    
+
     plot_data <- peptide_level_aggregation() %>%
       filter(`Cancer Type` %in% input$cancer_type_dropdown,
              assay %in% c("FT", "IgB")) %>%
       group_by(ID, assay, group, `Cancer Type`) %>%
       summarise(mean_has_target_PTM = mean(has_target_PTM, na.rm = TRUE)) %>%
       ungroup()
-    
+
     plot_title <- paste("Distribution of Proportion of Modified Peptide with",
                         ptm_info$name, "on site", ptm_info$site)
     create_histogram_plot(plot_data,
@@ -226,19 +242,19 @@ server <- function(input, output, session) {
                           ylab = "Frequency",
                           facet_formula = "~ assay")
   })
-  
+
   # Plot 6: Protein-level histogram by Cancer Type
   output$prot_prop_plot_cancer_type <- renderPlotly({
     req(protein_level_aggregation(), input$cancer_type_dropdown, input$file_dropdown)
     ptm_info <- TARGET_PTM[[input$file_dropdown]]
-    
+
     plot_data <- protein_level_aggregation() %>%
       filter(`Cancer Type` %in% input$cancer_type_dropdown,
              assay %in% c("FT", "IgB")) %>%
       group_by(ID, assay, group, `Cancer Type`) %>%
       summarise(mean_has_target_PTM = mean(has_target_PTM, na.rm = TRUE)) %>%
       ungroup()
-    
+
     plot_title <- paste("Distribution of Proportion of Modified Protein with",
                         ptm_info$name, "on site", ptm_info$site)
     create_histogram_plot(plot_data,
@@ -250,70 +266,70 @@ server <- function(input, output, session) {
                           ylab = "Frequency",
                           facet_formula = "~ assay")
   })
-  
-  
+
+
   # Plot 7: batch effect plot
   output$total_plate_batch_effect_plot <- renderPlotly({
     req(total_intensity_data(), input$file_dropdown)
     ptm_info <- TARGET_PTM[[input$file_dropdown]]
-    
-    p <- total_intensity_data() %>% group_by(ID, plate) %>% 
+
+    p <- total_intensity_data() %>% group_by(ID, plate) %>%
       summarise(log_total_intensity = log10(sum(total_intensity)), .groups = 'drop') %>%
       ggplot(aes(x = factor(plate), y = log_total_intensity)) +
       geom_violin(trim = FALSE, fill = "skyblue", alpha = 0.5) +  # Violin plot for distribution
       geom_boxplot(width = 0.1, outlier.shape = NA) +  # Boxplot inside violin
-      theme_minimal() +  
+      theme_minimal() +
       labs(x = "Batch ID", y = "log10(Total intensity)", title = "Total intensity (all peptide + all assay) of samples grouped by plate") +
       theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate x-axis labels if needed
-    
+
     # Convert to interactive plotly plot, enabling hover only for outliers
     ggplotly(p, tooltip = "text")
   })
-  
+
   # Plot 8: batch effect plot
   output$plate_batch_effect_plot <- renderPlotly({
     req(total_intensity_data(), input$file_dropdown)
     ptm_info <- TARGET_PTM[[input$file_dropdown]]
-    
-    p <- total_intensity_data() %>% 
+
+    p <- total_intensity_data() %>%
       ggplot(aes(x = factor(plate), y = log_total_intensity)) +
       geom_violin(trim = FALSE, fill = "skyblue", alpha = 0.5) +  # Violin plot for distribution
       geom_boxplot(width = 0.1, outlier.shape = NA) +  # Boxplot inside violin
       facet_wrap(~assay + has_target_PTM,
                  scales = "free_y",
                  labeller = labeller(has_target_PTM = c("TRUE" = "Modified peptides", "FALSE" = "Unmodified peptides"))) +
-      theme_minimal() +  
+      theme_minimal() +
       labs(x = "Batch ID", y = "log10(Total intensity)", title = "Total intensity of samples grouped by plate") +
       theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate x-axis labels if needed
-    
+
     # Convert to interactive plotly plot, enabling hover only for outliers
     ggplotly(p, tooltip = "text")
   })
-  
+
   output$violin_plot_conditions <- renderPlotly({
     req(total_intensity_data())  # Ensure data is available
-    
+
     # Create Summation across Condition B (grouping by A)
     intensity_by_modification <- total_intensity_data() %>%
       group_by(ID, has_target_PTM, plate) %>%  # Condition A
       summarise(total_intensity_sum = sum(total_intensity, na.rm = TRUE), .groups = "drop")
-    
+
     # Create Summation across Condition A (grouping by B)
     intensity_by_assay <- total_intensity_data() %>%
       group_by(ID, assay, plate) %>%  # Condition B (Modified vs. Unmodified)
       summarise(total_intensity_sum = sum(total_intensity, na.rm = TRUE), .groups = "drop")
-    
+
     # Violin Plot for Condition A
     p1 <- ggplot(intensity_by_modification, aes(x = as.factor(plate), y = log10(total_intensity_sum))) +
       geom_violin(trim=FALSE, alpha = 0.5, fill='skyblue') +
       geom_boxplot(width = 0.1, outlier.shape = NA) +
       labs(title = "Total intensity (IgB + FT) of samples grouped by plate",
            x = "Plate", y = "Total Intensity") +
-      facet_wrap(~has_target_PTM, 
+      facet_wrap(~has_target_PTM,
                  scales = "free_y",
                  labeller = labeller(has_target_PTM = c("TRUE" = "Modified peptides", "FALSE" = "Unmodified peptides"))) +
       theme_minimal()
-    
+
     # Violin Plot for Condition B
     p2 <- ggplot(intensity_by_assay, aes(x = as.factor(plate), y = log10(total_intensity_sum))) +
       geom_violin(trim=FALSE, alpha = 0.5, fill = "skyblue") +
@@ -322,26 +338,29 @@ server <- function(input, output, session) {
            x = "Plate", y = "Total Intensity") +
       facet_wrap(~assay, scales = "free_y") +
       theme_minimal()
-    
+
     # Convert ggplots to Plotly and display both
-    subplot(ggplotly(p1), ggplotly(p2), nrows = 2, shareX = TRUE) 
+    subplot(ggplotly(p1), ggplotly(p2), nrows = 2, shareX = TRUE)
   })
-  
-  
+
+
   #-------------------------modified peptide level------------------------------
   output$peptide_table <- DT::renderDataTable({
-      
+    req(input$cancer_type_dropdown_output)
+
     # select column
-    selected_table <- wide_format() %>% 
-      select(1:5, generated_metadata()$Run)
-    
+    selected_table <- annotated_data()$get_data(
+      assay = input$assay_dropdown_output,
+      cancer_type=input$cancer_type_dropdown_output,
+      normalization=input$normalization_dropdown_output)
+
     DT::datatable(selected_table,
                   options = list(
                     scrollX = TRUE
                   ))
-    
+
   })
-  
+
   output$download_peptide <- downloadHandler(
     filename = function() {
       paste("peptide_level_", input$file_dropdown, '_', Sys.Date(), ".csv", sep = "")
@@ -349,12 +368,12 @@ server <- function(input, output, session) {
     content = function(file) {
       # Assuming peptide_data() is the reactive that contains your data for the table.
       # You can also use the data directly if it isn't reactive.
-      selected_table <- wide_format() %>% 
+      selected_table <- wide_format() %>%
         select(1:5, generated_metadata()$Run)
       write.csv(selected_table, file, row.names = FALSE)
     }
   )
-  
+
   output$download_metadata <- downloadHandler(
     filename = function() {
       paste("metadata_", Sys.Date(), ".csv", sep = "")
--- diann_dashboard/ui.R
+++ diann_dashboard/ui.R
@@ -8,53 +8,53 @@ ptmAnalysisTab <- tabPanel(
   title = "Overview",
   fluidPage(
     titlePanel("PTM Analysis in Shiny"),
-    
+
     # Sidebar (Filters) at the Top
     fluidRow(
       column(4,
              selectInput("file_dropdown", "Select PTM File",
                          choices = names(TARGET_PTM),
                          selected = "Citrullination_test")),
-      column(4, 
-             sliderInput("PTM_qvalue_slider", "PTM Q-value threshold", min=0, max=0.1, 
+      column(4,
+             sliderInput("PTM_qvalue_slider", "PTM Q-value threshold", min=0, max=0.1,
                          value=0.95, step = 0.01)),
-      column(4, 
-             sliderInput("PTM_site_confidence_slider", "Location confidence", min=0, max=1, 
+      column(4,
+             sliderInput("PTM_site_confidence_slider", "Location confidence", min=0, max=1,
                          value=0, step = 0.01)),
 
     ),
-    
+
     # Main Panel with Plots
     fluidRow(
-      column(12, plotlyOutput("matplotlib_plot"))
+      column(12, plotlyOutput("intensity_plot"))
     ),
-    
+
     fluidRow(
       column(12, plotlyOutput('pep_prop_plot_case_control'))
     ),
-    
+
     fluidRow(
       column(12, plotlyOutput('prot_prop_plot_case_control'))
     ),
-    
+
     fluidRow(
       column(8,
-             selectInput("cancer_type_dropdown", "Select Cancer Types", 
+             selectInput("cancer_type_dropdown", "Select Cancer Types",
                          choices = NULL, selected = c('Breast', 'MERIT Control'), multiple = TRUE)
       ),
       column(4,
              sliderInput("bins_slider", "Number of Bins", min = 10, max = 100, value = 30, step = 1)
       )
     ),
-    
+
     fluidRow(
-      column(12, plotlyOutput("histogram_plot"))
+      column(12, plotlyOutput("intensity_plot_cancer_type"))
     ),
-    
+
     fluidRow(
       column(12, plotlyOutput('pep_prop_plot_cancer_type'))
     ),
-    
+
     fluidRow(
       column(12, plotlyOutput('prot_prop_plot_cancer_type'))
     )
@@ -74,9 +74,21 @@ batchEffect <- tabPanel(
 peptideLevel <- tabPanel(
   title = "Peptide level",
   fluidPage(
-    # dt table
-    selectInput("cancer_type_dropdown_output", "Select Cancer Types", 
-                       choices = NULL, selected = c('Breast', 'MERIT Control'), multiple = TRUE),
+    fluidRow(
+      # dt table
+      selectInput("cancer_type_dropdown_output", "Select Cancer Types",
+                  choices = NULL, selected = c('Breast', 'MERIT Control'), multiple = TRUE),
+      selectInput("assay_dropdown_output", "Assay",
+                  choices = c('FT', 'IgB'), selected = c('FT','IgB'), multiple = TRUE),
+      selectInput("normalization_dropdown_output", "normaliztion",
+                  choices = c('none', 'median', 'plate_median'), selected = 'none'),
+    ),
+
+    # warning
+    tags$div(
+      class = "alert alert-warning",
+      "Warning: normalization are performed separately for different assays."
+    ),
     DT::dataTableOutput("peptide_table"),
     downloadButton("download_peptide", "Download Data"),
     downloadButton("download_metadata", "Download Metadata")
